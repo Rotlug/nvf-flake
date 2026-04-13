349c282a3de6f346876f8673d5c7766f28832609
rotlug <rotlug77@gmail.com>
2026-04-13 11:55:45 +0300

    update find&replace function

--- lua/after.lua
+++ lua/after.lua
@@ -1,11 +1,21 @@
 -- Ctrl+F to find & replace
 vim.keymap.set("x", "<M-f>", function()
+	-- Yank the selection into register 'v'
 	vim.cmd('normal! "vy')
 	local sel = vim.fn.getreg("v"):gsub("\n", ""):gsub("\r", "")
-	sel = vim.fn.escape(sel, "/\\")
-	local base = ":%s/" .. sel .. "//g"
+
+	-- ESCAPING LOGIC:
+	-- We escape backslashes first, then the forward slash (delimiter),
+	-- then standard regex magic characters.
+	sel = vim.fn.escape(sel, "/\\^$*.[~")
+
+	-- Prepend '\V' to the search string to force "very nomagic" mode.
+	-- This makes most characters literal by default, adding a second layer of safety.
+	local base = ":%s/\\V" .. sel .. "//g"
+
 	local left = vim.api.nvim_replace_termcodes("<Left>", true, false, true)
 	local move = left .. left
 	local final = vim.api.nvim_replace_termcodes(base, true, false, true) .. move
+
 	vim.api.nvim_feedkeys(final, "n", false)
-end, { noremap = true, desc = "Global substitute of selected text (Alt-f)" })
+end, { noremap = true, desc = "Literal global substitute of selection (Alt-f)" })
